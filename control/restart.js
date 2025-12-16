3b7b46d5362195c11fa5dd8958daf8003acb2bb2
Aotumuri <Aotumuri.YT@gmail.com>
2025-12-16 20:41:26 +0900

    refactor: remove unused code

--- control/restart.js
+++ control/restart.js
@@ -1,12 +1,9 @@
 #!/usr/bin/env node
 import "dotenv/config";
-import fs from "fs";
-import path from "path";
 
 import { setDB } from "../server/db.js";
 import { startServer } from "../server/script.js";
 
-const SERVER_CONFIG_PATH = process.env.SERVER_CONFIG_PATH || "data/config.json";
 const DATABASE_PATH = process.env.DATABASE_PATH || "data/leaderboard.sqlite";
 
 setDB(DATABASE_PATH);
