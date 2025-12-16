edc76dafddea4885cbed446f7d6b18ed054f28e9
Aotumuri <Aotumuri.YT@gmail.com>
2025-12-16 20:23:40 +0900

    feat: add server restart functionality and test API endpoints

--- server/script.js
+++ server/script.js
@@ -2,6 +2,8 @@ import "dotenv/config";
 import http from "http";
 import fs from "fs";
 
+import { setDecoy, resetLeaderBoard } from "./db.js";
+
 const SERVER_CONFIG_PATH = process.env.SERVER_CONFIG_PATH || "data/config.json";
 
 // for stopping the servers (maintenance mode)
@@ -17,7 +19,12 @@ export function startServer() {
   for (const port_config of server_config) {
     // TODO: type によって分岐するようにする。
     const server = http.createServer((req, res) => {
-      res.end("Hello HTTP");
+      if (port_config.type === "api") {
+        apiServer(req, res, port_config);
+      } else {
+        // TODO: admin / client を作る
+        res.end("Hello HTTP");
+      }
     });
 
     server.listen(port_config.port, () => {
@@ -35,4 +42,38 @@ function stopServers() {
     s.close();
   }
   servers = [];
+}
+
+function apiServer(req, res, config) {
+  const { url, method } = req;
+  if (url.startsWith("/health") && method === "GET") {
+    responseApi(res, 200, "json", { status: "ok" });
+    return;
+  }
+
+  if (url.startsWith("/debug")) {
+    responseApi(res, 200, "json", setDecoy());
+    return;
+  }
+
+  if (url.startsWith("/reset")) {
+    resetLeaderBoard();
+    responseApi(res, 200, "json", { status: "ok" });
+    return;
+  }
+
+  responseApi(res, 404, "json", {error: "Not Found"});
+}
+
+function responseApi(res, status, type, payload) {
+  if (type === "json") {
+    res.writeHead(status, {"Content-Type": "application/json; charset=UTF-8"});
+    res.end(JSON.stringify(payload));
+  } else if (type === "text") {
+    res.writeHead(status, {"Content-Type": "text/plain; charset=UTF-8"});
+    res.end(payload);
+  } else {
+    res.writeHead(500);
+    res.end("Unknown response type");
+  }
 }
